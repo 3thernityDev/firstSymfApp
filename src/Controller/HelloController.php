a05097a566ba11c1467bd49e393b18109e4a6e0d
3thernitydev <3thernitydev@gmail.com>
2024-11-18 13:06:51 +0400

    first controller and vue

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HelloController extends AbstractController
+{
+    #[Route('/hello', name: 'app_hello')]
+    public function bidule()
+    {
+        return $this->render("hello/index.html.twig");
+    }
+}
