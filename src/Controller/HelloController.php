494af2edd2cbd8392d04df015ca85510f6b24f3b
3thernitydev <3thernitydev@gmail.com>
2024-11-18 13:23:01 +0400

    testing variables and routes

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -11,6 +11,12 @@ class HelloController extends AbstractController
     #[Route('/hello', name: 'app_hello')]
     public function bidule()
     {
-        return $this->render("hello/index.html.twig");
+        return $this->render("hello/index.html.twig", ["message" => "The end is near ⚠️"]);
+    }
+
+    #[Route('/mom', name: 'app_hello')]
+    public function mom()
+    {
+        return $this->render("hello/index.html.twig", ["message" => "Do you know where my mom is ? I think I lose her ... 😭"]);
     }
 }
